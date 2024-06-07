56dfb9030c53825feb6457c93b92c6399bc40901
Michael <explorerbrinegames@gmail.com>
2024-06-07 16:44:58 +0400

    first commit

---
+++ shop_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+ 
+ 
+
+ 
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Favourite)
+
+ 
